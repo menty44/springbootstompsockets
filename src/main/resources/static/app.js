4dda40359378c07a90e2e334ee61bf88c2314504
menty44 <menty44@gmail.com>
2019-05-09 15:59:05 +0300

    sockets using srping and stomp websocket library

---
+++ src/main/resources/static/app.js
@@ -0,0 +1,58 @@
+var stompClient = null;
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#userinfo").html("");
+}
+
+function connect() {
+    var socket = new SockJS('/websocket-example');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/user', function (greeting) {
+            showGreeting(JSON.parse(greeting.body).content);
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendName() {
+    stompClient.send("/app/user", {}, JSON.stringify({'name': $("#name").val()}));
+}
+
+function showGreeting(message) {
+    var name = localStorage.getItem('name');
+    console.log(message + ' ' + name);
+
+    if (name === message) {
+        $("#userinfo").append("<tr><td>" + message + "</td></tr>");
+    }else {
+        //$("#userinfobad").append("<tr><td>" + "bad" + "</td></tr>");
+    }
+
+}
+
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $( "#connect" ).click(function() { connect(); });
+    $( "#disconnect" ).click(function() { disconnect(); });
+    $( "#send" ).click(function() { sendName(); });
+});
